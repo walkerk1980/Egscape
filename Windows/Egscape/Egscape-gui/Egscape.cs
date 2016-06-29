217257b370a706d4b05440369076483a1554cf61
walkerk1980 <walkerk1980@gmail.com>
2016-06-28 18:38:47 -0600

    Added gui frame

---
+++ Windows/Egscape/Egscape-gui/Egscape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Egscape_gui
+{
+    class Egscape
+    {
+
+    }
+}
